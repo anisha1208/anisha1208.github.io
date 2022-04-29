31db13ee42b2218c8486248c1fd36623a03dfc1f
Anisha Agrawal <anishaagrawal1208@gmail.com>
2022-04-29 21:16:18 +0530

    Update

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navBarlinks = document.getElementsByClassName('navbar-links')[0]
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navBarlinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click',()=>{
-        navBarlinks.classList.toggle('active')
+// toggleButton.addEventListener('click',()=>{
+//         navBarlinks.classList.toggle('active')
+// })
+
+window.addEventListener('scroll',function(){
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky',window.scrollY>0);
 })
+
+function toggleMenu(){
+        var menuToggle = document.querySelector('.toggle');
+        var menuToggle = document.querySelector('.menu');
+        menuToggle.classList.toggle('active');
+        menu.classList.toggle('active');
+}
